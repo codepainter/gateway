c2f8687ed2988624182ff169ba87c73b4044027b
Abiyasa <abiyasa2410@gmail.com>
2019-10-09 19:49:58 +0700

    cleanup

--- plugins/support.js
+++ plugins/support.js
@@ -1,21 +1,9 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
+const fp = require("fastify-plugin");
 
-// the use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
-
-module.exports = fp(function (fastify, opts, next) {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
-  })
-  next()
-})
-
-// If you prefer async/await, use the following
-//
-// module.exports = fp(async function (fastify, opts) {
-//   fastify.decorate('someSupport', function () {
-//     return 'hugs'
-//   })
-// })
+module.exports = fp(async function(fastify, opts) {
+    fastify.decorate("someSupport", function() {
+        return "hugs";
+    });
+});
--- services/example/index.js
+++ services/example/index.js
@@ -1,17 +1,7 @@
-'use strict'
+"use strict";
 
-module.exports = function (fastify, opts, next) {
-  fastify.get('/example', function (request, reply) {
-    reply.send('this is an example')
-  })
-
-  next()
-}
-
-// If you prefer async/await, use the following
-//
-// module.exports = async function (fastify, opts) {
-//   fastify.get('/example', async function (request, reply) {
-//     return 'this is an example'
-//   })
-// }
+module.exports = async function(fastify, opts) {
+    fastify.get("/example", async function(request, reply) {
+        return "this is an example";
+    });
+};
